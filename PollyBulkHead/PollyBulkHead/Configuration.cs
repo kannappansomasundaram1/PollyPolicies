b09d3faf0eecc5f811a01edcac5dc94669288d36
KANNAPPANS <Kannappan.Somasundaram@thetrainline.com>
2023-03-15 10:46:06 +0000

    Bulkhead trial

---
+++ PollyBulkHead/PollyBulkHead/Configuration.cs
@@ -0,0 +1,98 @@
+﻿using System.Net.Http.Json;
+using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.Bulkhead;
+using Polly.Contrib.WaitAndRetry;
+using Polly.Extensions.Http;
+using Polly.Retry;
+using WeatherForeCastApi;
+
+namespace AmiUpdater;
+
+public static  class ConfigurationExtensions
+{
+    public static void ConfigureServices(this ServiceCollection serviceCollection)
+    {
+        serviceCollection.AddHttpClient<IWeatherClient, WeatherClient>(
+                client =>
+                {
+                    client.BaseAddress = new Uri("https://localhost:7159/");
+                })
+            .AddPolicyHandler(GetRetryPolicy())
+            .AddPolicyHandler(BuildBulkheadLimitAsyncPolicy(50, 20, 2));
+            
+        serviceCollection.AddTransient<PolicyTester>();
+    }
+    
+    static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+    {
+        return HttpPolicyExtensions
+            .HandleTransientHttpError()
+            .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+            .WaitAndRetryAsync(
+                Backoff.DecorrelatedJitterBackoffV2(medianFirstRetryDelay: TimeSpan.FromSeconds(20), 2),
+                onRetryAsync: (e, ts, i, ctx) =>
+                {
+                    Console.WriteLine($"Bulkhead Retry: {i}");
+                    return Task.CompletedTask;
+                });
+    }
+    
+    static AsyncPolicy<HttpResponseMessage> BuildBulkheadLimitAsyncPolicy(int maxParallelExecutions, int maxQueuingActions, int retryingNumber)
+    {
+        var asyncBulkheadPolicy = Policy.BulkheadAsync<HttpResponseMessage>(maxParallelExecutions, maxQueuingActions,
+            ctx =>
+            {
+                Console.WriteLine("Bulkhead Rejected");
+                return Task.CompletedTask;
+            });
+
+        return asyncBulkheadPolicy;
+    }
+
+    private static AsyncRetryPolicy AsyncRetryPolicy()
+    {
+        var sleepDurations = 
+            Backoff.DecorrelatedJitterBackoffV2(medianFirstRetryDelay: TimeSpan.FromSeconds(20), 2);
+        var retryPolicy = Policy
+            .Handle<BulkheadRejectedException>()
+            .WaitAndRetryAsync(
+                sleepDurations,
+                onRetryAsync: (e, ts, i, ctx) =>
+                {
+                    Console.WriteLine($"Bulkhead Retry: {i}");
+                    return Task.CompletedTask;
+                });
+        return retryPolicy;
+    }
+}
+
+public class WeatherClient : IWeatherClient
+{
+    private readonly HttpClient _httpClient;
+
+    public WeatherClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<WeatherForecast[]> GetForeCast()
+    {
+        try
+        {
+            var fromJsonAsync = await _httpClient.GetFromJsonAsync<WeatherForecast[]>("WeatherForecast");
+            Console.WriteLine("Successful Response");
+            return fromJsonAsync;
+        }
+        catch (BulkheadRejectedException e)
+        {
+            Console.WriteLine("Final BulkheadRejectedException");
+            return null;
+        }
+    }
+}
+
+public interface IWeatherClient
+{
+    Task<WeatherForecast[]> GetForeCast();
+}
---
+++ PollyBulkHead/PollyBulkHead/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using AmiUpdater;
+using Microsoft.Extensions.DependencyInjection;
+
+var serviceCollection = new ServiceCollection();
+serviceCollection.ConfigureServices();
+var serviceProvider = serviceCollection.BuildServiceProvider();
+var handler = serviceProvider.GetRequiredService<PolicyTester>();
+await handler.Test();
+
+public class PolicyTester
+{
+    private readonly IWeatherClient _client;
+
+    public PolicyTester(IWeatherClient client)
+    {
+        _client = client;
+    }
+    public async Task Test()
+    {
+        var stopwatch = Stopwatch.StartNew();
+        var tasks = Enumerable.Range(1, 100).Select(x => _client.GetForeCast()).ToList();
+        var foreCasts = await Task.WhenAll(tasks);
+        var failedCount = foreCasts.Count(x => x != null);
+        var successfulCount = foreCasts.Count(x => x == null);
+        Console.WriteLine(stopwatch.ElapsedMilliseconds);
+    }
+}
