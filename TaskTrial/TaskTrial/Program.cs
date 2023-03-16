7ad58c154e47797aa6b9e1cd0f3b16af067cab60
KANNAPPANS <Kannappan.Somasundaram@thetrainline.com>
2023-03-16 17:14:12 +0000

    Task trial

---
+++ TaskTrial/TaskTrial/Program.cs
@@ -0,0 +1,18 @@
+﻿var tasks = Enumerable.Range(0,100).Select(async numb => await Map(numb)).ToArray();
+
+var vortexEventWrappers = new List<string>();
+var batchSize = 20; 
+var mappedTasks = tasks.Chunk(batchSize).ToArray();
+foreach (var vortexEventsBatchTasks in mappedTasks)
+{
+    var mappedString = (await Task.WhenAll(vortexEventsBatchTasks)).Where(t => t != null).ToList();
+    vortexEventWrappers.AddRange(mappedString);
+}
+
+Console.WriteLine(vortexEventWrappers.Aggregate((a, b) => $"{a},{b}"));
+static async Task<string> Map(int i)
+{
+    await Task.Delay(0);
+    Console.WriteLine($"Calling External API to get string for {i}");
+    return i.ToString();
+}
